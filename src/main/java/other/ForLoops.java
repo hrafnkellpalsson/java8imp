729be8e2afc8ba60eace08147a9ae983dabd330a
Hrafnkell Palsson <hrafnkell.palsson@gmail.com>
2017-04-26 13:47:15 -0700

    Removed code not pertaining to book exercises

--- src/main/java/other/ForLoops.java
+++
@@ -1,7 +0,0 @@
-package other;
-
-class ForLoops {
-  // Demonstrate different kinds of for loops, enhanced, traditional, using casting, using iterators.
-  // Are there other variations?
-  // I believe an enhanced for loop is compiled to use an iterator?
-}
